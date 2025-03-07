3af16ded69aaa423ed74a6aa1507cfc2260564e9
fayjulislamalimran <fayjulislamalimran7@gmail.com>
2025-03-07 12:52:45 +0600

    If Else

---
+++ If Else/script.js
@@ -0,0 +1,11 @@
+if (3 < 10) {
+  console.log("I am small. I do not bite");
+}
+
+if (13 < 10) {
+  console.log("Smaller but stronger");
+}
+const biriyaniPrice = 500;
+if (biriyaniPrice < 300) {
+  console.log("Mama give me some biriyani");
+}
