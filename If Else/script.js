acdb97e269e6a132730ca97518f387c925b013b0
fayjulislamalimran <fayjulislamalimran7@gmail.com>
2025-03-09 12:09:17 +0600

    some uodate

--- If Else/script.js
+++ If Else/script.js
@@ -9,3 +9,74 @@ const biriyaniPrice = 500;
 if (biriyaniPrice < 300) {
   console.log("Mama give me some biriyani");
 }
+
+const rainingOutside = true;
+if (rainingOutside == true) {
+  console.log("You and me under the three");
+} else {
+  console.log("No rain, no romance");
+}
+
+const weight = 40;
+if (weight > 20) {
+  console.log("Rickshaw mama cholo jai");
+} else {
+  console.log("Walking is Exercising");
+}
+
+const comeBackHome = 5;
+if (comeBackHome >= 6) {
+  console.log("Nasta kaibay");
+} else {
+  console.log("Jaru bari miss nai");
+}
+
+const mobilePhone = "Login";
+if (mobilePhone == "Login") {
+  console.log("Welcome");
+} else {
+  console.log("Get Loss!");
+}
+
+const run = 5;
+if (run >= 5) {
+  console.log("Choklet kaibay");
+} else {
+  console.log("Buri barbe");
+}
+
+const myMark = 85;
+if (myMark > 80) {
+  console.log("Bike Pabo");
+} else {
+  console.log("Bike nai kopal o");
+}
+
+const moveShow = 9;
+if (moveShow < 9) {
+  console.log("Move dekmu");
+} else {
+  console.log("Move dekbo na");
+}
+
+const tapmatra = 10;
+if (tapmatra >= 30) {
+  console.log("Condisonar Cholbe");
+} else {
+  console.log("Cholbe na");
+}
+
+/*const salary = 75000;
+if (salary > 50000) {
+  console.log("Tmi patro");
+} else {
+  console.log("Next 5 year single thako");
+} */
+
+const salary = 63000;
+const isBCS = false;
+if (salary > 5000 && isBCS == true) {
+  console.log("Sei lavel er patro");
+} else {
+  console.log("Dore giya moro");
+}
