b2760f9cfb7d14e81b05dd087d2fbc9740495aac
fayjulislamalimran <fayjulislamalimran7@gmail.com>
2025-03-10 17:29:10 +0600

    It Else Done

--- If Else/script.js
+++ If Else/script.js
@@ -80,3 +80,322 @@ if (salary > 5000 && isBCS == true) {
 } else {
   console.log("Dore giya moro");
 }
+
+/*const mySalary = 25000;
+const height = 68;
+if (mySalary > 25000 || height > 72) {
+  console.log("Bolo baba kobul");
+} else {
+  console.log("Bag tui mokbul");
+}
+*/
+// Practice
+const age = 20;
+const height = 60;
+if (age > 18 && height > 60) {
+  console.log("Gari telabe");
+} else {
+  console.log("Bose bose khabe");
+}
+
+const math = 70;
+const eng = 70;
+if (math > 80 || eng > 85) {
+  console.log("Borthi hote parbe");
+} else {
+  console.log("Borthi hower juggo na");
+}
+
+const gpa = 4;
+const monthlyIncome = 2000;
+if (gpa == 5 && monthlyIncome == 10000) {
+  console.log("Scholarship pabe");
+} else {
+  console.log("Scholarship pabe na");
+}
+
+const shakriPartiAge = 20;
+const workEx = 4;
+if (shakriPartiAge < 30 && workEx > 2) {
+  console.log("Chakri porika dite parbe");
+} else {
+  console.log("Chakri porika dite parbe na");
+}
+
+const egg = 20;
+const isChiken = true;
+if (egg > 12 || isChiken == true) {
+  console.log("Dime er kurma ranna hobe");
+} else {
+}
+console.log("pawruti kaw");
+
+const manTemperature = 100;
+const isKasi = true;
+if (manTemperature > 100 || isKasi == true) {
+  console.log("Dr dekaw");
+} else {
+  console.log("Muri kaw");
+}
+
+const studentPesn = 80;
+const homeWorkDone = true;
+if (studentPesn > 80 && homeWorkDone == true) {
+  console.log("Exm dite parbe");
+} else {
+  console.log("Auto fail");
+}
+
+const currentAblebl = true;
+const mobileChargeDown = true;
+
+if (currentAblebl == true && mobileChargeDown == true) {
+  console.log("Porte bosbo");
+} else {
+  console.log("Game kelbo");
+}
+
+const shirtPrice = 1000;
+const kupon = true;
+if (shirtPrice > 1000 && kupon == true) {
+  console.log("20 %");
+} else {
+  console.log("0 %");
+}
+
+/*const price = 6500;
+if (price >= 5000) {
+  const discount = (price / 100) * 10;
+  const pay = price - discount;
+  console.log(pay);
+} else {
+  console.log(price);
+}*/
+
+// const price = 4000;
+// if (price >= 5000) {
+//   const discount = (price / 100) * 10;
+//   const pay = price - discount;
+//   console.log(pay);
+// } else if (price >= 2500) {
+//   const discount = (price / 100) * 5;
+//   const pay = price - discount;
+//   console.log(pay);
+// } else {
+//   console.log(price);
+// }
+
+const friendBuy = 4500;
+if (friendBuy >= 3000) {
+  const discount = (friendBuy / 100) * 5;
+  const friendPay = friendBuy - discount;
+  console.log(friendPay);
+} else if (friendBuy >= 6000) {
+  const discount = (friendBuy / 100) * 15;
+  const friendPay = friendBuy - discount;
+  console.log(friendPay);
+} else {
+  console.log(friendBuy);
+}
+
+const boyAge = 70;
+if (boyAge <= 12) {
+  console.log("Free");
+} else if (boyAge >= 60) {
+  console.log("50%");
+} else {
+  console.log("Full pay");
+}
+
+const bankBlance = 4400;
+if (bankBlance <= 100) {
+  console.log("Tk kom");
+} else if (bankBlance >= 1000 && bankBlance <= 5000) {
+  // >= 1000 && bankBlance <= 5000
+  console.log("Bindas life");
+} else {
+  console.log("Tmi doni miya biya koro");
+}
+
+const mark = 80;
+if (mark < 50) {
+  console.log("Fail");
+} else if (mark >= 50 && mark <= 80) {
+  console.log("Pass");
+} else if (mark > 80) {
+  console.log("A+");
+} else {
+  console.log("Write a number");
+}
+
+const bookPage = 200;
+if (bookPage < 100) {
+  console.log("Small Book");
+} else if (bookPage >= 100 && bookPage < +500) {
+  console.log("Mid size book");
+} else if (bookPage > 500) {
+  console.log("Heart attack size book");
+} else {
+  console.log("Mama erro");
+}
+
+const relTemperature = 1;
+if (relTemperature < 0) {
+  console.log("Ice");
+} else if (relTemperature > 0 && relTemperature <= 20) {
+  console.log("Cool cool");
+} else if (relTemperature > 20) {
+  console.log("Hot hot");
+} else {
+  console.log("Erro");
+}
+
+const playerLavel = 51;
+if (playerLavel < 10) {
+  console.log("Novice");
+} else if (playerLavel >= 10 && playerLavel <= 50) {
+  console.log("Expert");
+} else if (playerLavel > 50) {
+  console.log("Pro Gamer");
+} else {
+  console.log(Erro);
+}
+
+const money = 300;
+const popcornPrice = 40;
+if (money >= 300) {
+  console.log("Going to watch a movie");
+  if (popcornPrice <= 50) {
+    console.log("Buying PopCorn");
+  } else {
+    console.log("PopCorn is Expensive");
+  }
+} else {
+  console.log("Home alone");
+}
+
+const friendInvite = true;
+const pocktMany = 10000;
+if (friendInvite == true) {
+  console.log("Ami biya kanit jaimu");
+  if (pocktMany >= 1000) {
+    console.log("Gift paibay");
+  } else {
+    console.log("Gift nai");
+  }
+} else {
+  console.log("Biyar dawt deco na listo jayga nai tumer");
+}
+
+/*যদি তোর বাসায় মেহমান আসে, প্রথমে জিজ্ঞেস করবি, তারা চা খাবে কি না।
+যদি চা খেতে চায়, তাহলে জিজ্ঞেস করবি, সাথে বিস্কুট খাবে কি না। না চাইলে
+বলবি, 'শুধু চা রেডি' আর যদি চা-ও না খেতে চায়, বলবি, 'বসে বসে 
+জলসা দেখুন'*/
+
+const wantsTea = true;
+const wantsBiscuit = false;
+
+if (wantsTea == true) {
+  if (wantsBiscuit == false) {
+    console.log("Cha biscut kaw");
+  } else {
+    console.log("Cha kaw");
+  }
+} else {
+  console.log("Star jolsa deko");
+}
+
+const appActive = true;
+const subscriptions = true;
+if (appActive == true) {
+  if (subscriptions == true) {
+    console.log("Pro version deko");
+  } else {
+    console.log("Free deko");
+  }
+} else {
+  console.log("Active nay");
+}
+
+const frizKabarAce = true;
+const appWorking = false;
+if (frizKabarAce == true) {
+  if (appWorking == true) {
+    console.log("Order now");
+  } else {
+    console.log("Ami ruja");
+  }
+} else {
+  console.log("Kabar gorom morbo");
+}
+
+const gast = 100;
+const payGift = true;
+if (gast >= 100) {
+  if (payGift == true) {
+    console.log("Let party all night");
+  } else {
+    console.log("I will party with myself");
+  }
+} else {
+  console.log("100 jon asbe na");
+}
+
+let isLeader = true;
+if (isLeader === true) {
+  console.log("Govermment money is my money");
+}
+
+if (isLeader) {
+  console.log("Govermment money is my money");
+}
+
+const isPassed = false;
+if (isPassed == false) {
+  console.log("Ajke khawa dawa bondo");
+}
+if (!isPassed) {
+  console.log("Ajke khawa dawa bondo");
+}
+
+const rakibAge = 20;
+if (rakibAge >= 18) {
+  console.log("Vote for the hot");
+} else {
+  console.log("You are not eligible to vote");
+}
+rakibAge >= 18 ? console.log("Eligible") : console.log("Not");
+
+let price = 500;
+let isLeaders = false;
+if (isLeaders == false) {
+  price = 0;
+} else {
+  price = price + 100;
+}
+
+price = isLeaders === true ? 0 : price + 100;
+
+let cashBack = 200;
+cashBack >= 3000 ? console.log(500) : console.log(0);
+
+let imranAge = 60;
+imranAge >= 15 ? console.log("Teenager") : console.log("Child");
+
+const isLoggedin = true;
+isLoggedin == true ? console.log("Welcome Back") : console.log("Please login");
+
+const telFull = true;
+telFull == true
+  ? console.log("Ready for a long drive")
+  : console.log("Fill the tank");
+
+const exmPass = true;
+
+exmPass == true ? console.log("Party time") : console.log("Next semester ....");
+
+const sunny = false;
+sunny == false ? console.log("Stay Home") : console.log("Erro");
+
+const expensive = false;
+expensive == false ? console.log("I will buy this item") : console.log("");
