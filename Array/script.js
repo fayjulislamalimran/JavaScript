d17db10f726d6f4907d6b6d58a3d979a65648ce3
fayjulislamalimran <fayjulislamalimran7@gmail.com>
2025-03-10 22:06:32 +0600

    Arry

---
+++ Array/script.js
@@ -0,0 +1,24 @@
+// const numbers = [11, 12, 13, 14];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const marks = [82, 76, 54, 45];
+const friends = ["Abul", "Kabul", "Tabul"];
+const passed = [true, false, true];
+const mixedArray = [12, "Dim", true];
+
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const size = myNum.length;
+console.log(size);
+
+// Practice
+const newArry = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+const fol = ["Apple", "Komla", "Amm"];
+const sobjiName = ["Alu", "Potol", "Kumra", "Data"];
+console.log(sobjiName.length);
+
+const moveName = ["Ma amer jan", "Tanda matar kuni", "Kuti tk kabin"];
+// const bijur = [11, 13, 15, 17, 19, 2, 25, 27, 29];
+const colorName = ["Red", "Green ", "Yellow"];
+const cityName = ["Nabiganj", "Habiganj"];
+
+const bijur = [11, 13, 15, 17, 19, 2, 25, 27, 29];
+console.log(bijur[1]);
