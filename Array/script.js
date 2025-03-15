014b433c9d5e314fb2dd4fb0ddd84f3446885fb2
fayjulislamalimran <fayjulislamalimran7@gmail.com>
2025-03-15 15:39:18 +0600

    Loop

--- Array/script.js
+++ Array/script.js
@@ -1,5 +1,5 @@
 // const numbers = [11, 12, 13, 14];
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const marks = [82, 76, 54, 45];
 const friends = ["Abul", "Kabul", "Tabul"];
 const passed = [true, false, true];
@@ -22,3 +22,97 @@ const cityName = ["Nabiganj", "Habiganj"];
 
 const bijur = [11, 13, 15, 17, 19, 2, 25, 27, 29];
 console.log(bijur[1]);
+
+//  Practice
+const friendName = ["Rakib", "Riku", "Sojib", "Dipu"];
+console.log(friendName[3]);
+
+const favBook = ["Ban", "Eng", "Mat", "Ruk", "JS", "Hablu"];
+console.log(favBook[5]);
+
+const someNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+someNum[7] = 30;
+console.log(someNum);
+
+const game = ["FF", "Pubge", "Candy", "Tempolrun"];
+game[2] = "Sapar";
+console.log(game);
+
+const familyMember = ["Ripon", "Sumon", "Rimon", "Imran", "Emlak"];
+console.log(familyMember[3]);
+
+const visit = ["Uk", "USA", "Ca", "UAE", "BD", "IR", "NEZ"];
+console.log(visit[4]);
+
+const numbers = [12, 87, 98, 45];
+numbers.push(44);
+console.log(numbers);
+
+const newNumbers = [12, 87, 98, 45];
+newNumbers.push(44, 65, 98, 11, 9);
+console.log(newNumbers);
+
+const updatFriends = ["Balam", "Kalam", "Salam"];
+console.log(updatFriends);
+
+const friend = ["Balam", "Kalam", "Salam", "Gelam", "Pailam"];
+// friend.pop();
+// friend.shift()
+friend.unshift("Imran");
+
+console.log(friend);
+
+// Practice
+const priceNum = [10, 20, 30, 40, 50];
+priceNum.push(60);
+console.log(priceNum);
+
+const newFriend = ["Sojib", "Sagor", "Sakib", "Suhel"];
+newFriend.push("Sumon");
+console.log(newFriend);
+
+const myGame = ["FF", "Pubji", "Football"];
+myGame.pop();
+console.log(myGame);
+
+const tewNewNum = [24, 36, 48, 60];
+tewNewNum.unshift(12);
+console.log(tewNewNum);
+
+const book = ["Book1", "Book2", "Book3", "Book4", "Book5"];
+book.shift();
+console.log(book);
+
+// const friendsUpdate = ["Balam", "Kalam", "Salam", "Gelam", "Pailam"];
+// const exist = friendsUpdate.includes("Balam");
+// console.log(exist);
+/* const friendsUpdate = ["Balam", "Kalam", "Salam", "Gelam", "Pailam"];
+if (friendsUpdate.includes("Balam")) {
+  console.log("Party");
+} else {
+  console.log("No food, We are fasting");
+} */
+
+const friendsUpdate = ["Balam", "Kalam", "Salam", "Gelam", "Pailam"];
+const index = friend.indexOf("Pailam");
+console.log(index);
+
+const deshiPol = ["Amm", "Kola", "Apple", "Lichu"];
+if (deshiPol.includes("Amm")) {
+  console.log("Amm ace");
+} else {
+  console.log("Amm nai");
+}
+
+const newArray = ["Babul", "Alif", "Chuton"];
+console.log(newArray.indexOf("Babul"));
+
+const myNewFriend = ["Rimon", "Rifat", "Rajib"];
+console.log(myNewFriend.indexOf("Rifat"));
+
+const city = ["Dhaka", "Chittagong", "Sylhet", "rajshahi"];
+if (city.includes("RajShahi")) {
+  console.log("Ace");
+} else {
+  console.log("Nai");
+}
---
+++ Loop/script.js
@@ -0,0 +1,109 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["Orange", "Apple", "Banana", "Jackfruit", "Watermelon"];
+for (const item of fruits) {
+  console.log(item);
+}
+
+// Practice
+const fiveSub = ["Book1", "Book2", "Book3", "Book4", "Book5"];
+for (const bookItems of fiveSub) {
+  console.log(bookItems);
+}
+const myFavFood = ["Tea", "Orange"];
+for (const favFood of myFavFood) {
+  console.log(favFood);
+}
+
+const footballPlayer = ["Messi", "Runaldu"];
+for (const player of footballPlayer) {
+  console.log(player);
+}
+
+/*let num = 0;
+while (num < 5) {
+  console.log(num);
+  num = num + 1;
+}*/
+
+let num = 0;
+while (num < 5) {
+  console.log("Bsmboo Eating High School");
+  num++;
+}
+
+let tenTime = 0;
+while (tenTime < 10) {
+  console.log("I am writing progmmer 3 houres");
+  tenTime++;
+}
+
+let addNum = 0;
+while (addNum < 21) {
+  console.log(addNum);
+  addNum++;
+}
+
+let numPrint = 50;
+while (numPrint <= 100) {
+  console.log(numPrint);
+  numPrint++;
+}
+
+let numUpdat = 110;
+while (numUpdat <= 111) {
+  console.log(numUpdat / 2);
+  numUpdat++;
+}
+
+let num = 1;
+while (num < 10) {
+  console.log(num);
+  num++;
+}
+
+let sum = 0;
+let num = 1;
+while (num <= 10) {
+  console.log(num);
+  sum = num + sum;
+  num++;
+}
+console.log(sum);
+
+// Practice
+let num = 50;
+let sum = 0;
+while (num <= 100) {
+  // console.log(num);
+  sum = sum + num;
+  num++;
+}
+console.log(sum)
+
+let num = 5;
+let sum = 0;
+while (num <= 15) {
+  // console.log(num);
+  sum = sum + num;
+  num++;
+}
+console.log(sum);
+
+let studentNum = 1;
+let sumNumber = 0;
+while (studentNum <= 50) {
+  // console.log(studentNum);
+  studentNum++;
+  sumNumber = studentNum + sumNumber;
+}
+console.log(sumNumber);
+
+let num = 21;
+while (num <= 50) {
+  console.log(num);
+  num++;
+}
